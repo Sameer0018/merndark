d234ea7892d0296c4ccd69d7c90b60d0c739e623
Sameer Hussain <87130217+Sameer0018@users.noreply.github.com>
2021-12-20 10:10:37 +0530

    Update Footer.js

--- server/client/src/components/Footer.js
+++ server/client/src/components/Footer.js
@@ -31,7 +31,7 @@ const Footer = () => {
                     <i className="icon ion-social-facebook"></i></a>
                     <a href="#"><i className="icon ion-social-twitter"></i>
                     </a><a href="#"><i className="icon ion-social-snapchat"></i>
-                    </a><a href="https://www.instagram.com/22._.piyush/"><i className="icon ion-social-instagram"></i>
+                    </a><a href="https://www.instagram.com/sameer_._hussain_._/"><i className="icon ion-social-instagram"></i>
                     </a></div>
 
             </div>
